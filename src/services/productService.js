2a47cbae192b134f16f8d2ecfcfa43eb1baf294f
Cristian Ferreiro Montoiro <cristian.ferreiro@udc.es>
2025-10-19 20:11:43 +0200

    Added service

--- src/services/productService.js
+++ src/services/productService.js
@@ -5,3 +5,15 @@ const API_URL = "https://itx-frontend-test.onrender.com/api";
 export const getProducts = () => {
     return axios.get(`${API_URL}/product`);
 }
+
+export const getProduct = (id) => {
+    return axios.get(`${API_URL}/product/${id}`);
+}
+
+export const addProductToCart = (id, colorCode, storageCode) => {
+    return axios.post(`${API_URL}/cart`, {
+        id: id,
+        colorCode: colorCode,
+        storageCode: storageCode
+    });
+}
