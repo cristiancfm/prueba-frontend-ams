c32ec6416a438f7f1b09829216e8b3b6a295a83c
Cristian Ferreiro Montoiro <cristian.ferreiro@udc.es>
2025-10-20 22:56:47 +0200

    Fixed horizontal scrolling

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -4,12 +4,12 @@ import Header from "../components/Header.jsx";
 
 function MainLayout() {
     return (
-        <Box sx={{ display: "flex", height: "100vh", width:"100vw", flexDirection: "column", overflowX: "hidden" }}>
+        <div className="App">
             <Header />
             <Container sx={{ flex: 1, py: 4 }}>
                 <Outlet />
             </Container>
-        </Box>
+        </div>
     );
 }
 
