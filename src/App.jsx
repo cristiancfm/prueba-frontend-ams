09097f90ca9e4f50e282533131535bb36c41c06f
Cristian Ferreiro Montoiro <cristian.ferreiro@udc.es>
2025-10-20 19:33:30 +0200

    Navigate to product list when accessing home

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import MainLayout from "./layout/MainLayout.jsx";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
 import ProductList from "./pages/ProductList.jsx";
 import NotFound from "./pages/NotFound.jsx";
-import Home from "./pages/Home.jsx";
 import ProductDetail from "./pages/ProductDetail.jsx";
 
 const router = createBrowserRouter([
@@ -11,7 +10,7 @@ const router = createBrowserRouter([
         handle: { crumb: "Home" },
         element: <MainLayout />,
         children: [
-            { index: true, element: <Home /> },
+            { index: true, element: <Navigate to="/products" replace /> },
             { path: "products", handle: { crumb: "Products" }, element: <ProductList /> },
             { path: "products/:id", handle: { crumb: "Product Detail" }, element: <ProductDetail />, },
             { path: "*", handle: { crumb: "Not Found" }, element: <NotFound /> },
